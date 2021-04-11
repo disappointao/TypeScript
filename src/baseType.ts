e390b6183af2f677d15ea85afab01990f1ddfd14
liuchuangao <1147608791@qq.com>
2021-04-11 22:53:19 +0800

    [补充][数组]

--- src/baseType.ts
+++ src/baseType.ts
@@ -15,8 +15,14 @@ var1 = false
 let numOrString: number | string = 123
 numOrString = 'str'
 
+//数组泛型
 let arr: Array<number> = [123,345]
 let arr2: number[] = [123,345]
+//接口表示数组
+interface numberArr{
+    [index: number]:number
+}
+let arr3: numberArr = [0,1,2,3,4]
 arr2.push(2)
 
 let user: [string, number] = ['name', 18]
